9ebb9da6d3b294df1b790a345518683f84a63c84
Alex Burkhart <alex@estuary.dev>
2022-04-25 14:03:19 -0400

    Run tests and build image in CI

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Build Stage
+################################################################################
+FROM golang:1.18-buster as builder
+
+WORKDIR /builder
+
+# Download & compile dependencies early. Doing this separately allows for layer
+# caching opportunities when no dependencies are updated.
+COPY go.* ./
+RUN go mod download
+
+# Build the gateway.
+COPY main.go ./
+COPY gen ./gen
+RUN go build .
+
+
+# Runtime Stage
+################################################################################
+FROM gcr.io/distroless/base-debian10
+
+WORKDIR /app
+ENV PATH="/app:$PATH"
+
+# Bring in the compiled artifact from the builder.
+COPY --from=builder /builder/data-plane-gateway ./data-plane-gateway
+
+# Avoid running the data-plane-gateway as root.
+USER nonroot:nonroot
+
+ENTRYPOINT ["/app/data-plane-gateway"]
--- test.sh
+++ test.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# -e causes the script to exit on encountering an error
-# -m turns on job management, required for our use of `fg` below.
-set -em
+set -e
+
+function bail() {
+    log "$@" 1>&2
+    exit 1
+}
+
+function log() {
+  echo "[test.sh] ${1}"
+}
 
 # The first arg sets the MODE. There are two MODES:
 # 1. "server" mode will launch flow, launch the gateway, and deploy a
@@ -12,15 +19,26 @@ set -em
 # the typescript tests with deno. When these finish, the test server will
 # shutdown.
 MODE="${1:-server}"
-echo "MODE: $MODE"
+log "MODE: $MODE"
+
+# The second arg sets the GATEWAY_BIN. This is the full path to the gateway binary to
+# use. This allows local developers to use their local build/installation of
+# the gateway, while CI can build a fresh copy immediately before running tests.
+GATEWAY_BIN="${2}"
+if [ -z "${GATEWAY_BIN}" ]; then
+  GATEWAY_BIN=$(which data-plane-gateway)
+fi
+
+# The third arg sets the FLOW_BIN. This is the full path to the flow binary to
+# use. This allows local developers to use their local build/installation of
+# flowctl, while CI can download the flowctl binary to a known location.
+FLOW_BIN="${3}"
+if [ -z "${FLOW_BIN}" ]; then
+  FLOW_BIN=$(which flowctl)
+fi
 
 ROOT_DIR="$(git rev-parse --show-toplevel)"
-cd "$ROOT_DIR"
-
-function bail() {
-    echo "$@" 1>&2
-    exit 1
-}
+cd "${ROOT_DIR}"
 
 TESTDIR="test/tmp"
 
@@ -44,67 +62,73 @@ export GATEWAY_ADDRESS=localhost:${GATEWAY_PORT}
 export BUILD_ID=test-build-id
 export CATALOG_SOURCE="test/acmeCo/source-hello-world.flow.yaml"
 
-echo "TESTDIR setup: ${TESTDIR}"
+log "TESTDIR setup: ${TESTDIR}"
 
 # Start an empty local data plane within our TESTDIR as a background job.
 # --poll so that connectors are polled rather than continuously tailed.
 # --sigterm to verify we cleanly tear down the test catalog (otherwise it hangs).
 # --tempdir to use our known TESTDIR rather than creating a new temporary directory.
 # --unix-sockets to create UDS socket files in TESTDIR in well-known locations.
-# ${TESTDIR}/flowctl temp-data-plane \
-flowctl temp-data-plane \
+${FLOW_BIN} temp-data-plane \
   --log.level info \
   --poll \
   --tempdir=${TESTDIR} \
   --unix-sockets \
+  --sigterm \
   &
 DATA_PLANE_PID=$!
 
-echo "Data plane launched: ${DATA_PLANE_PID}"
+log "Data plane launched: ${DATA_PLANE_PID}"
 
 # Start the gateway and point it at the data plane
-go run . \
+${GATEWAY_BIN} \
   --gateway-address=${GATEWAY_ADDRESS} \
   --broker-address=${BROKER_ADDRESS} \
   --consumer-address=${CONSUMER_ADDRESS} \
   &
 GATEWAY_PID=$!
 
-echo "Gateway launched: ${GATEWAY_PID}"
+log "Gateway launched: ${GATEWAY_PID}"
 
 # Arrange to stop the background processes on exit.
 trap "kill -s SIGTERM ${DATA_PLANE_PID} \
    && kill -s SIGTERM ${GATEWAY_PID} \
    && wait ${DATA_PLANE_PID} \
-   && wait ${GATEWAY_PID}" \
+   && wait ${GATEWAY_PID} \
+   && exit 0" \
    EXIT
 
 # Build the catalog.
-# ${TESTDIR}/flowctl api build \
-flowctl api build \
+${FLOW_BIN} api build \
   --directory=${TESTDIR}/builds \
   --build-id=${BUILD_ID} \
-  --source=${CATALOG_SOURCE} \
-  --ts-package || bail "Build failed."
+  --source=${CATALOG_SOURCE} || bail "Build failed."
 
-echo "Build finished"
+log "Build finished"
 
 # Activate the catalog.
-# ${TESTDIR}/flowctl api activate \
-flowctl api activate \
+${FLOW_BIN} api activate \
   --build-id=${BUILD_ID} \
   --all \
   --log.level=info || bail "Activate failed."
 
 
-echo "Activation finished"
+log "Activation finished"
 
 if [ "${MODE}" == "run" ]; then
   # Wait just a bit longer for the shard to boot up.
   sleep 5
 
-  cd client
-  npm test
+  make test
+
+  # Tests pass, so let's cleanup the test catalog so the data plane exits cleanly.
+  ${FLOW_BIN} api delete \
+  --build-id=${BUILD_ID} \
+  --all \
+  --log.level=info || bail "Delete failed."
+
+  log "Test Passed"
 else
+  log "Ready"
   wait
 fi
