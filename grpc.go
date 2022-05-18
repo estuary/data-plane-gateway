d5f2026fc1fed5721dddecd97aa7b81eae813610
Alex Burkhart <alex@estuary.dev>
2022-05-18 16:01:26 -0400

    Multiplex GRPC+REST
    Sets up the GRPC services for handling auth, without actually doing any
    auth yet. This just proxies the request straight to the downstream
    gazette services.

---
+++ grpc.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	context "context"
+	"io"
+	"net"
+	"net/url"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
+)
+
+func dialAddress(ctx context.Context, addr string) (*grpc.ClientConn, error) {
+	dialCtx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	var dialAddr string
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	if url, err := url.Parse(addr); err != nil {
+		return nil, err
+	} else if url.Scheme == "unix" {
+		dialAddr = url.Path
+		opts = append(opts, grpc.WithDialer(func(addr string, timeout time.Duration) (net.Conn, error) {
+			return net.DialTimeout("unix", addr, timeout)
+		}))
+	} else {
+		dialAddr = url.String()
+	}
+
+	conn, err := grpc.DialContext(dialCtx, dialAddr, opts...)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		<-ctx.Done()
+		if cerr := conn.Close(); cerr != nil {
+			grpclog.Infof("Failed to close conn to %s: %v", addr, cerr)
+		}
+	}()
+
+	return conn, err
+}
+
+/// This is a bit reversed from normal operations. We're forwarding messages
+/// from the local grpc server to a remote server.  Sends messages received by
+/// the server to the client and sends responses sent by the client to the
+/// server.
+func proxyStream(ctx context.Context, source grpc.ServerStream, destination grpc.ClientStream, req interface{}, resp interface{}) error {
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				// Keep going
+			}
+			err := source.RecvMsg(req)
+			if err == io.EOF {
+				return nil
+			} else if err != nil {
+				return err
+			}
+			err = destination.SendMsg(req)
+			if err == io.EOF {
+				return nil
+			} else if err != nil {
+				return err
+			}
+		}
+	})
+	eg.Go(func() error {
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				// Keep going
+			}
+			err := destination.RecvMsg(resp)
+			if err == io.EOF {
+				return nil
+			} else if err != nil {
+				return err
+			}
+			err = source.SendMsg(resp)
+			if err == io.EOF {
+				return nil
+			} else if err != nil {
+				return err
+			}
+		}
+	})
+
+	return eg.Wait()
+}
--- main.go
+++ main.go
@@ -3,8 +3,16 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/soheilhy/cmux"
+	pb "go.gazette.dev/core/broker/protocol"
+	pc "go.gazette.dev/core/consumer/protocol"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -16,15 +24,81 @@ var (
 
 func main() {
 	flag.Parse()
-	var err error
 
-	srv := NewRestServer()
-	go srv.TrapShutdownSignal(15 * time.Second)
+	journalServer := NewJournalAuthServer()
+	shardServer := NewShardAuthServer()
 
-	// Start HTTP server (and proxy calls to gRPC server endpoint)
-	err = srv.Serve(*gatewayAddr)
+	grpcServer := grpc.NewServer()
+	pb.RegisterJournalServer(grpcServer, journalServer)
+	pc.RegisterShardServer(grpcServer, shardServer)
+
+	mux := NewGracefulMux()
+	go mux.ServeGrpc(*grpcServer)
+	go mux.ServeHttp(NewRestServer())
+	go mux.TrapShutdownSignal(5 * time.Second)
+
+	log.Printf("Listening on %s\n", *gatewayAddr)
+	err := mux.Serve()
 	if err != nil {
-		log.Fatalf("Failed to serve rest gateway: %v", err)
+		log.Fatalf("Failed to serve: %v", err)
 	}
+
 	os.Exit(0)
+}
+
+type GracefulMux struct {
+	mux      cmux.CMux
+	shutdown chan bool
+}
+
+func NewGracefulMux() *GracefulMux {
+	listener, err := listen(*gatewayAddr)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	return &GracefulMux{
+		mux:      cmux.New(listener),
+		shutdown: make(chan bool, 0),
+	}
+}
+
+func (s *GracefulMux) ServeGrpc(server grpc.Server) {
+	// TODO: figure out why the content-type matcher doesn't work.
+	// listener := s.mux.Match(cmux.HTTP2HeaderField("content-type", "application/grpc"))
+	listener := s.mux.Match(cmux.HTTP2())
+
+	server.Serve(listener)
+}
+
+func (s *GracefulMux) ServeHttp(handler http.Handler) {
+	listener := s.mux.Match(cmux.HTTP1Fast())
+	server := &http.Server{Handler: handler}
+
+	server.Serve(listener)
+}
+
+func (s *GracefulMux) Serve() error {
+	err := s.mux.Serve()
+	if err != nil {
+		return err
+	}
+
+	// Wait for shutdown to complete
+	<-s.shutdown
+
+	return nil
+}
+
+func (s *GracefulMux) TrapShutdownSignal(timeout time.Duration) error {
+	var signaled = make(chan os.Signal, 1)
+	signal.Notify(signaled, syscall.SIGTERM, syscall.SIGINT)
+	<-signaled
+
+	s.mux.Close()
+
+	// Signal that we've finished shutting down
+	close(s.shutdown)
+
+	return nil
 }
