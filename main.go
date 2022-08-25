2591f711080807c507678d7332cc663725f16e28
Phil <phil@estuary.dev>
2022-08-25 00:54:37 -0400

    Support terminating TLS in the data-plane-gateway
    Adds TLS support to the server using a provided certificate and key.
    TLS is of course optional, and disabled if no certificate argument is
    given.

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	context "context"
+	"crypto/tls"
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
@@ -21,6 +24,8 @@ var (
 	jwtVerificationKey = flag.String("verification-key", "supersecret", "Key used to verify JWTs signed by the Flow Control Plane")
 	port               = flag.String("port", "28318", "Service port for HTTP and gRPC requests. A random port is used if not set. Port may also take the form 'unix:///path/to/socket' to use a Unix Domain Socket")
 	zone               = flag.String("zone", "local", "Availability zone within which this process is running")
+	tls_cert           = flag.String("tls-certificate", "", "Path to the TLS certificate (.crt) to use.")
+	tls_private_key    = flag.String("tls-private-key", "", "The private key for the TLS certificate")
 )
 
 var corsConfig *corsSettings
@@ -32,7 +37,35 @@ func main() {
 
 	pb.RegisterGRPCDispatcher(*zone)
 
-	srv, err := server.New("", *port)
+	var listener, err = net.Listen("tcp", fmt.Sprintf(":%s", *port))
+	if err != nil {
+		log.Fatalf("failed to start listener: %v", err)
+	}
+	// If TLS is enabled, then wrap the above listener in one that handles TLS termination.
+	if *tls_cert != "" {
+		if *tls_private_key == "" {
+			log.Fatalf("must supply --tls-private-key with --tls-certificate")
+		}
+
+		crt, err := tls.LoadX509KeyPair(*tls_cert, *tls_private_key)
+		if err != nil {
+			log.Fatalf("failed to load tls certificate: %v", err)
+		}
+
+		var config = tls.Config{
+			Certificates: []tls.Certificate{crt},
+			// NextProtos is someone's clever name for the list of supported ALPN protocols.
+			// We need to explicitly configure support for HTTP2 here, or else it won't be offered.
+			NextProtos: []string{"h2", "http/1.1"},
+		}
+		listener = tls.NewListener(listener, &config)
+	} else {
+		if *tls_private_key != "" {
+			log.Fatalf("must supply --tls-certificate with --tls-private-key")
+		}
+	}
+
+	srv, err := server.NewFromListener(listener)
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
