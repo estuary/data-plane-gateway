72ec24e82ea86c69fbc89bfb919cda45542d2da9
Phil <phil@estuary.dev>
2022-09-08 10:30:09 -0400

    First pass at automatic TLS cert provisioning
    Adds support for provisioning TLS certificates automatically using the
    `autocert` package. This uses etcd as a cache for the certificate
    and intermediate data used during provisioning. It also uses etcd for a
    distributed mutex, to prevent multiple data-plane-gateway processes from
    starting the certificate provisioning process simultaneously.
    
    A plain (non-TLS) listener was also added in order to respond to the
    ACME http-01 challenges.

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 	pb "go.gazette.dev/core/broker/protocol"
 	pc "go.gazette.dev/core/consumer/protocol"
 	"go.gazette.dev/core/task"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 )
 
@@ -26,10 +24,16 @@ var (
 	consumerAddr       = flag.String("consumer-address", "localhost:9000", "Target consumer address")
 	corsOrigin         = flag.String("cors-origin", "*", "CORS Origin")
 	jwtVerificationKey = flag.String("verification-key", "supersecret", "Key used to verify JWTs signed by the Flow Control Plane")
-	port               = flag.String("port", "28318", "Service port for HTTP and gRPC requests. A random port is used if not set. Port may also take the form 'unix:///path/to/socket' to use a Unix Domain Socket")
+	plainPort          = flag.String("plain-port", "28317", "Port for unencrypted communication")
+	tlsPort            = flag.String("port", "28318", "Service port for HTTP and gRPC requests. A random port is used if not set. Port may also take the form 'unix:///path/to/socket' to use a Unix Domain Socket")
 	zone               = flag.String("zone", "local", "Availability zone within which this process is running")
 	tls_cert           = flag.String("tls-certificate", "", "Path to the TLS certificate (.crt) to use.")
 	tls_private_key    = flag.String("tls-private-key", "", "The private key for the TLS certificate")
+
+	// Args that are required for automatically acquiring TLS certificates
+	autoAcquireCert      = flag.String("auto-tls-cert", "dpg-domain-name.example", "Automatically acquire TLS certificate from Let's Encrypt for the given domain using the ACME protocol")
+	etcdEndpoint         = flag.String("etcd-endpoint", "localhost:2379", "comma separated list of ETCD endpoints to connect to for managing TLS certificates. Only used when auto-tls-cert argument is provided")
+	autoAcquireCertEmail = flag.String("tls-cert-email", "", "email address to associate with the automatically acquired TLS certificate")
 )
 
 var corsConfig *corsSettings
@@ -40,13 +44,62 @@ func main() {
 	corsConfig = NewCorsSettings(*corsOrigin)
 
 	pb.RegisterGRPCDispatcher(*zone)
+	var grpcServer = grpc.NewServer(
+		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
+		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+	)
 
-	var listener, err = net.Listen("tcp", fmt.Sprintf(":%s", *port))
+	ctx := pb.WithDispatchDefault(context.Background())
+	var tasks = task.NewGroup(ctx)
+
+	journalServer := NewJournalAuthServer(ctx)
+	shardServer := NewShardAuthServer(ctx)
+	pb.RegisterJournalServer(grpcServer, journalServer)
+	pc.RegisterShardServer(grpcServer, shardServer)
+
+	// Will be used with both http and https
+	var healthHandler = http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("OK\n"))
+	})
+
+	plainMux := http.NewServeMux()
+	plainMux.Handle("/healthz", healthHandler)
+
+	httpsMux := http.NewServeMux()
+	restHandler := NewRestServer(ctx, fmt.Sprintf("localhost:%s", *tlsPort))
+	httpsMux.Handle("/healthz", healthHandler)
+	httpsMux.Handle("/", restHandler)
+
+	// compose both http and grpc into a single handler, that dispatches each request based on
+	// the content-type. It's important that we do this on a per-request basis instead of a
+	// per-connection basis because this server may be behind a load balancer, which will maintain
+	// persistent connections that get re-used for both protocols. This approach was taken from:
+	// https://ahmet.im/blog/grpc-http-mux-go/
+	var mixedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("content-type"), "application/grpc") {
+			grpcServer.ServeHTTP(w, r)
+		} else {
+			httpsMux.ServeHTTP(w, r)
+		}
+	})
+
+	var plainServer = &http.Server{
+		Addr: fmt.Sprintf(":%s", *plainPort),
+	}
+	var httpsServer = &http.Server{
+		//Handler: h2c.NewHandler(mixedHandler, &http2Server),
+		Handler: mixedHandler,
+	}
+
+	// This plain listener will be wrapped in one that does TLS termination. The implementation will
+	// depend on whether the TLS cert was provided directly versus us provisioning it automatically.
+	var tlsListener, err = net.Listen("tcp", fmt.Sprintf(":%s", *tlsPort))
 	if err != nil {
 		log.Fatalf("failed to start listener: %v", err)
 	}
-	// If TLS is enabled, then wrap the above listener in one that handles TLS termination.
 	if *tls_cert != "" {
+		// Plain http will only serve the health check
+		plainServer.Handler = plainMux
 		if *tls_private_key == "" {
 			log.Fatalf("must supply --tls-private-key with --tls-certificate")
 		}
@@ -62,58 +115,48 @@ func main() {
 			// We need to explicitly configure support for HTTP2 here, or else it won't be offered.
 			NextProtos: []string{"h2", "http/1.1"},
 		}
-		listener = tls.NewListener(listener, &config)
+		tlsListener = tls.NewListener(tlsListener, &config)
+	} else if *autoAcquireCert != "" {
+
+		var etcdUrls = getEtcdUrls()
+		certProvider, err := InitAutocert(*autoAcquireCert, etcdUrls, *autoAcquireCertEmail)
+		if err != nil {
+			panic(fmt.Sprintf("initializing autocert: %v", err))
+		}
+		tlsListener = tls.NewListener(tlsListener, certProvider.TLSConfig())
+		// Plain http will respond to ACME http-01 challenges and health checks.
+		plainServer.Handler = certProvider.acManager.HTTPHandler(plainMux)
 	} else {
-		if *tls_private_key != "" {
-			log.Fatalf("must supply --tls-certificate with --tls-private-key")
-		}
+		log.Fatalf("server requires TLS: must supply either --tls-certificate --tls-private-key, or --auto-tls-cert --etcd-endpoint")
 	}
-	var grpcServer = grpc.NewServer(
-		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
-		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
-	)
 
-	tasks := task.NewGroup(context.Background())
-	ctx := pb.WithDispatchDefault(tasks.Context())
-
-	journalServer := NewJournalAuthServer(ctx)
-	shardServer := NewShardAuthServer(ctx)
-	pb.RegisterJournalServer(grpcServer, journalServer)
-	pc.RegisterShardServer(grpcServer, shardServer)
-
-	httpMux := http.NewServeMux()
-	restHandler := NewRestServer(ctx, listener.Addr().String())
-
-	httpMux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("OK\n"))
-	}))
-	httpMux.Handle("/", restHandler)
-
-	// compose both http and grpc into a single handler, that dispatches each request based on
-	// the content-type. It's important that we do this on a per-request basis instead of a
-	// per-connection basis because this server may be behind a load balancer, which will maintain
-	// persistent connections that get re-used for both protocols. This approach was taken from:
-	// https://ahmet.im/blog/grpc-http-mux-go/
-	var mixedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.ProtoMajor == 2 && strings.HasPrefix(r.Header.Get("content-type"), "application/grpc") {
-			grpcServer.ServeHTTP(w, r)
-		} else {
-			httpMux.ServeHTTP(w, r)
-		}
+	tasks.Queue("http server", func() error {
+		return plainServer.ListenAndServe()
 	})
 
-	// Whether we're running with TLS or not, we need to use http2 in order for grpc to work.
-	// The `h2c.NewHandler` enables us to use http2 without tls.
-	var http2Server = http2.Server{}
-	var http1Server = &http.Server{Handler: h2c.NewHandler(mixedHandler, &http2Server)}
+	tasks.Queue("https server", func() error {
+		return httpsServer.Serve(tlsListener)
+	})
+	tasks.GoRun()
 
-	log.Printf("Listening on %s\n", listener.Addr().String())
+	log.Printf("Listening on %s\n", tlsListener.Addr().String())
 
-	err = http1Server.Serve(listener)
+	err = tasks.Wait()
 	if err != nil {
 		log.Fatalf("Failed to serve: %v", err)
 	}
 
 	log.Println("goodbye")
 	os.Exit(0)
+}
+
+func getEtcdUrls() []string {
+	var etcdUrls = strings.Split(*etcdEndpoint, ",")
+
+	for _, url := range etcdUrls {
+		if len(url) == 0 {
+			panic(fmt.Sprintf("invalid etcd urls argument: '%s', value must be a non-empty comma-separated list of URLs", *etcdEndpoint))
+		}
+	}
+	return etcdUrls
 }
