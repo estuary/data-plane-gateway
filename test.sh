1dacc8004ac5b2d8e1a30f4d897eeb422dc4d9d8
Joseph Shearer <joseph@josephshearer.net>
2022-12-08 16:12:57 -0500

    fix: `flowctl-admin` -> `flowctl-go`

--- test.sh
+++ test.sh
@@ -37,7 +37,7 @@ fi
 # flowctl, while CI can download the flowctl binary to a known location.
 FLOW_BIN="${3}"
 if [ -z "${FLOW_BIN}" ]; then
-  FLOW_BIN="$(command -v flowctl-admin)"
+  FLOW_BIN="$(command -v flowctl-go)"
 fi
 
 ROOT_DIR="$(git rev-parse --show-toplevel)"
